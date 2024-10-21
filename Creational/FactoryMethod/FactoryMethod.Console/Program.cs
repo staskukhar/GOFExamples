d34f5f1d5e895a55e566f3d4610e34745ee86254
Stas Kukhar <staskuk2005@gmail.com>
2024-10-21 16:17:56 +0300

    adds example of using factory method

---
+++ Creational/FactoryMethod/FactoryMethod.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using FactoryMethod.DesignPattern.Creators;
+using FactoryMethod.DesignPattern.Figures;
+
+void ProcessFigureCreator(IFigureCreator creator)
+{
+    IFigure figure = creator.CreateFigureWithDefaultMeasures();
+    Console.WriteLine($"Figure: {figure}");
+    Console.WriteLine($"Area: {figure.CalculateArea():N2}");
+}
+
+ProcessFigureCreator(new SquareCreator());
+ProcessFigureCreator(new CircleCreator());
