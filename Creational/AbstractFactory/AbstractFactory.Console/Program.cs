ed8af01f514247ae72a31e465c8f6a286d823d88
Stas Kukhar <staskuk2005@gmail.com>
2024-10-26 19:00:34 +0300

    adds example of usage

---
+++ Creational/AbstractFactory/AbstractFactory.Console/Program.cs
@@ -0,0 +1,17 @@
+﻿using AbstractFactory.DesignPattern.Abstraction;
+using AbstractFactory.DesignPattern.Implementation;
+
+ProcessMealFactory(new MorningSetFactory());
+ProcessMealFactory(new LunchSetFactory());
+ProcessMealFactory(new EveningSetFactory());
+
+void ProcessMealFactory(IMealSetFactory mealFactory)
+{
+    Console.WriteLine($"Meal factory: {mealFactory.GetType().Name}");
+    
+    Console.WriteLine(
+        mealFactory.GetDrink().GetDrinkDescription());
+    Console.WriteLine(
+        mealFactory.GetMeal().GetMealDescription());
+    Console.WriteLine();
+}
