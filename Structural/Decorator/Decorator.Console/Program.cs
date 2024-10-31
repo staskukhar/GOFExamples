54110fa35dbc064e57e8b8274c12c8f6a2230033
Stas Kukhar <staskuk2005@gmail.com>
2024-11-01 00:00:00 +0200

    adds usage example

---
+++ Structural/Decorator/Decorator.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using Decorator.DesignPattern.Abstraction;
+using Decorator.DesignPattern.Implementation;
+
+IPaymentCard plainCard = new PaymentCard();
+plainCard.ProcessPayment(400m);
+Console.WriteLine();
+
+IPaymentCard cardPlus = new PaymentCardPlus();
+cardPlus.ProcessPayment(200m);
+Console.WriteLine();
+
+IPaymentCard premiumCard = new PaymentCardPremium();
+premiumCard.ProcessPayment(800m);
