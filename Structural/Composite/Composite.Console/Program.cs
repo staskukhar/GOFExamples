7cdcbebb6a9d049c2511e73a17a407cbe8e249ac
Stas Kukhar <staskuk2005@gmail.com>
2024-11-01 20:03:15 +0200

    adds usage example

---
+++ Structural/Composite/Composite.Console/Program.cs
@@ -0,0 +1,31 @@
+﻿using Composite.DesignPattern.Abstraction;
+using Composite.DesignPattern.Implementation;
+
+
+//                                 -- subTreeOfFirstSubTree(4, 2)
+//                                / 
+// cyberTree -> firstSubTree(5) --
+//                                \
+//                                 -- anotherSubTreeOfFirstSubTree(8, 6)
+// total = 5 + (4 + 2) + (8 + 6)
+
+CyberTree cyberTree = new CyberTree();
+
+// Subtree - 1
+CyberTree firstSubTree = new CyberTree();
+firstSubTree.AddNestedComponent(new FruitBunch(5));
+cyberTree.AddNestedComponent(firstSubTree);
+
+// Subtree - 1.1
+CyberTree subTreeOfFirstSubTree = new CyberTree();
+subTreeOfFirstSubTree.AddNestedComponent(new FruitBunch(4));
+subTreeOfFirstSubTree.AddNestedComponent(new FruitBunch(2));
+firstSubTree.AddNestedComponent(subTreeOfFirstSubTree);
+
+// Subtree - 1.2
+CyberTree anotherSubTreeOfFirstSubTree = new CyberTree();
+subTreeOfFirstSubTree.AddNestedComponent(new FruitBunch(8));
+subTreeOfFirstSubTree.AddNestedComponent(new FruitBunch(6));
+firstSubTree.AddNestedComponent(anotherSubTreeOfFirstSubTree);
+
+Console.WriteLine($"Cyber tree has {cyberTree.GetNumberOfFruits()} fruits.");
