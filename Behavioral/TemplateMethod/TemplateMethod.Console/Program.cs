71baddb3cccf34362bf87f805d2e7b38a356e80d
Stas Kukhar <staskuk2005@gmail.com>
2024-11-05 22:16:47 +0200

    adds usage example

---
+++ Behavioral/TemplateMethod/TemplateMethod.Console/Program.cs
@@ -0,0 +1,9 @@
+﻿using TemplateMethod.DesignPattern.Abstraction;
+using TemplateMethod.DesignPattern.Implementation;
+
+Browser chrome = new Chrome();
+Browser edge = new Edge();
+
+chrome.Search("Template method design pattern");
+Console.WriteLine();
+edge.Search("Template method design pattern");
