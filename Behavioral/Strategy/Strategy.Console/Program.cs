2f98512054511c2168ea2591510a9881faa724de
Stas Kukhar <staskuk2005@gmail.com>
2024-11-12 00:18:19 +0200

    adds usage example

---
+++ Behavioral/Strategy/Strategy.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿using Strategy.DesignPattern.Abstraction;
+using Strategy.DesignPattern.Implementation;
+
+IList<int> numbers = [2, 4, 5, 6];
+AvgComputer computer = new AvgComputer(new AvgThroughMinMaxStrategy());
+
+Console.WriteLine($"Avg through min/max strategy: {computer.GetAvg(numbers)}");
+computer.Strategy = new AvgThroughAllStrategy();
+Console.WriteLine($"Avg through all strategy: {computer.GetAvg(numbers)}");
+computer.Strategy = new AvgAsMedianStrategy();
+Console.WriteLine($"Avg as median strategy: {computer.GetAvg(numbers)}");
